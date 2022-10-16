e97db53b6fb00e742444fe28b7f99b9c465d5760
TyrantLink <tyrantlink@pm.me>
2022-10-16 06:14:05 -0700

    added benchmark and readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# arguments
+|arg|description|
+|--|--|
+|none|runs with random seed until reaching 20 (so basically forever)|
+|--benchmark|runs a benchmark with 1,873,503,784 iterations|
+|--high-acc-benchmark|runs a benchmark with 12,007,207,237 iterations|
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,76 @@
-use rand::Rng;
 use time::Instant;
+use rand::prelude::*;
+use rand_chacha::ChaCha8Rng;
+use std::{env,process::exit};
 
-fn main()
+static GOAL:i8 = 20; // has to be hard-coded, for some reason taking user input makes it ~33% slower
+
+fn actual_run()
 {
-	let mut rng = rand::thread_rng();
-	let goal:i16 = 20; // has to be hard-coded, for some reason taking user input makes it ~33% slower
-	let rgoal:i16 = goal+1; // for some reason, using 1..=goal in the random function makes it run ~55% slower
-	let mut total_highest:i16 = 0;
-	let mut instance_highest:i16 = 0;
+  let mut rng:ChaCha8Rng = ChaCha8Rng::from_entropy();
+	let rgoal:i8 = GOAL+1;
+	let mut total_highest:i8 = 0;
+	let mut instance_highest:i8 = 0;
 	let mut iters:u128 = 0;
 	let start:Instant = Instant::now();
 
-	while instance_highest != goal {
+	while instance_highest != GOAL
+  {
 		iters += 1;
-		if rng.gen_range(1..rgoal) == instance_highest+1 {
+		if rng.gen_range(1..rgoal) == instance_highest+1
+    {
 			instance_highest += 1;
-			if instance_highest > total_highest {
+			if instance_highest > total_highest
+      {
 				total_highest = instance_highest;
-				println!("{}/{} in {} seconds and {} iters | {} it/s",total_highest,goal,start.elapsed().as_seconds_f32(),iters,iters as f64/start.elapsed().as_seconds_f32() as f64);
+				println!("{}/{} in {} seconds and {} iters | {} it/s",total_highest,GOAL,start.elapsed().as_seconds_f32(),iters,iters as f64/start.elapsed().as_seconds_f64());
 			}
 		} else {instance_highest = 0;}
 	}
+}
+
+fn benchmark(acc:i8)
+{
+  let mut rng:ChaCha8Rng = ChaCha8Rng::seed_from_u64(7);
+	let rgoal:i8 = GOAL+1;
+	let mut total_highest:i8 = 0;
+	let mut instance_highest:i8 = 0;
+	let mut iters:u128 = 0;
+	let start:Instant = Instant::now();
+
+	while instance_highest != GOAL
+  {
+		iters += 1;
+		if rng.gen_range(1..rgoal) == instance_highest+1
+    {
+			instance_highest += 1;
+			if instance_highest > total_highest
+      {
+				total_highest = instance_highest;
+        print!("");
+        if total_highest == acc
+        {
+          println!("benchmark complete: {} iters/s",iters as f64/start.elapsed().as_seconds_f64());
+          exit(0);
+        }
+			}
+		} else {instance_highest = 0;}
+	}
+}
+
+fn main()
+{
+  let arg:String = env::args().nth(1).unwrap_or("".to_string());
+  
+  if arg == "--benchmark"
+  {
+    println!("running benchmark...");
+    benchmark(7)
+  }
+  else if arg == "--high-acc-benchmark"
+  {
+    println!("running high accuracy benchmark...");
+    benchmark(8)
+  }
+  else {actual_run()}
 }
