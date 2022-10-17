9b59094b27171913e2e3bc81594af8b30082d6a2
TyrantLink <tyrantlink@pm.me>
2022-10-16 18:26:53 -0700

    added support for multiple run locations added commas to ouput values

--- src/multi_benchmark.py
+++ src/multi_benchmark.py
@@ -2,26 +2,28 @@ from subprocess import Popen,PIPE
 from os.path import exists
 
 
-if not exists('target/release/stupid-die-roller'):
+FILE_PATH = f'{"../" if exists("main.rs") else ""}target/release/stupid-die-roller'
+
+if not exists(FILE_PATH):
 	print('file not found, please run `cargo build --release`')
 
 
 def benchmark(high_acc:bool):
-	proc = Popen(['target/release/stupid-die-roller',f'--{"high-acc-" if high_acc else ""}benchmark'],stdout=PIPE,stdin=PIPE)
+	proc = Popen([FILE_PATH,f'--{"high-acc-" if high_acc else ""}benchmark'],stdout=PIPE,stdin=PIPE)
 	proc.stdin.write(b'20\n')
 	proc.stdin.flush()
 	while True:
 		line = proc.stdout.readline().decode()
 		if line.startswith('benchmark complete:'):
 			result = float(line.split(' ')[2])
-			print(f'{result} rolls/s')
+			print(f'{"{:,}".format(round(result,2))} rolls/s')
 			return result
 
 def main():
 	high_acc = input('run high accuracy benchmark? [y/N] ').lower() in ['y','yes','true']
 	loops = int(input('how many times would you like to run the benchmark? [5]') or 5)
 	benchmarks = [benchmark(high_acc) for i in range(loops)]
-	print(f'{"[HIGH-ACC] " if high_acc else ""}average of {loops} benchmarks: {sum(benchmarks)/loops}')
+	print(f'{"[HIGH-ACC] " if high_acc else ""}average of {loops} benchmarks: {"{:,}".format(round(sum(benchmarks)/loops,2))} rolls/s')
 	
 if __name__ == '__main__':
 	main()
